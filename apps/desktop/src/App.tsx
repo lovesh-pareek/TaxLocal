f1a2fac9e94307db7fa285ec09bff7b3311eb18d
lovesh-pareek <loveshpareeklk@gmail.com>
2026-06-14 22:38:46 +0530

    Create App component for TaxLocal application

---
+++ apps/desktop/src/App.tsx
@@ -0,0 +1,10 @@
+export function App() {
+  return (
+    <div>
+      <h1>TaxLocal</h1>
+      <p>Offline-first Income Tax Return Preparation Platform</p>
+    </div>
+  );
+}
+
+export default App;
