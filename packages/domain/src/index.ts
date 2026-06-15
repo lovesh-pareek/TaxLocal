1b3812d49742f01d3d52b6d9cc20f1c34e7b83c5
Lavesh Pareek <>
2026-06-15 17:15:43 +0530

    TASK-004 add vite and tauri configuration

--- packages/domain/src/index.ts
+++ packages/domain/src/index.ts
@@ -1,5 +1,5 @@
-export * from "./entities/TaxWorksheet";
 export * from "./entities/TaxReturn";
+export * from "./entities/TaxWorksheet";
 export * from "./entities/UserProfile";
 export * from "./entities/AuditEvent";
 export * from "./entities/ValidationError";
